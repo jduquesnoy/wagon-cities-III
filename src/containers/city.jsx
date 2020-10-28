b731a363343ab9b13ae72db97d723b8a177fb789
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-10-28 15:00:13 +0100

    code cleaning

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class City extends Component { // A stateful component needs to
- constructor(props) { // be promoted into a class
- super(props);
- this.state = {}; // defines initial state
- }
+  constructor(props) { // be promoted into a class
+    super(props);
+    this.state = {}; // defines initial state
+  }
 
- render() {
-  return (<li className="list-group-item">
-            {this.props.city.name}</li>);
- }
+  render() {
+    return (<li className="list-group-item">
+      {this.props.city.name}</li>);
+  }
 }
 
 export default City;
